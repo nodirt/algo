47c1ee62ef3ce45c4c3064a3a48557cf1912195e
nodirt <turakulov@gmail.com>
2013-07-13 14:14:15 -0400

    Minor refactoring in RepeatedTestRunner

--- test/algo/RepeatedTestRunner.java
+++ test/algo/RepeatedTestRunner.java
@@ -1,7 +1,6 @@
 package algo;
 
 import org.junit.internal.runners.statements.InvokeMethod;
-import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
