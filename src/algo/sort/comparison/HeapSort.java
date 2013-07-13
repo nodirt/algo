0f3794ded0e5cc73acf07b132977cb80b71d9285
nodirt <turakulov@gmail.com>
2013-07-13 14:13:41 -0400

    heapsort

---
+++ src/algo/sort/comparison/HeapSort.java
@@ -0,0 +1,55 @@
+package algo.sort.comparison;
+
+public class HeapSort<E> extends ComparisonSorting<E> {
+    int xx;
+    
+    @Override
+    public void sort(final E[] array) {
+        
+        class Heap {
+            int size = array.length;
+
+            int getLeft(int index) {
+                return index * 2;
+            }
+            int getRight(int index) {
+                return index * 2 + 1;
+            }
+            
+            void maxHeapify(int index) {
+                while (true) {
+                    int left = getLeft(index);
+                    int right = getRight(index);
+                    int largest = index;
+                    if (left < size && compare(array[left], array[largest]) > 0) {
+                        largest = left;
+                    }
+
+                    if (right < size && compare(array[right], array[largest]) > 0) {
+                        largest = right;
+                    }
+
+                    if (largest == index) {
+                        break;
+                    }
+                    swap(array, largest, index);
+                    index = largest;
+                }
+            }
+
+            void sort() {
+                for (int i = array.length / 2; i >= 0; i--) {
+                    maxHeapify(i);
+                }
+
+                for (int i = array.length - 1; i > 0; i--) {
+                    swap(array, 0, i);
+                    size--;
+                    maxHeapify(0);
+                }
+            }
+        }
+
+        new Heap().sort();
+    }
+}
