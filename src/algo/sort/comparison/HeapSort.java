69688bd96ccdc61089e23dc4713163f10e03abfd
nodirt <turakulov@gmail.com>
2013-07-15 00:35:38 -0400

    ArrayUtil and RandomUtil

--- src/algo/sort/comparison/HeapSort.java
+++ src/algo/sort/comparison/HeapSort.java
@@ -1,5 +1,7 @@
 package algo.sort.comparison;
 
+import static algo.util.ArrayUtil.*;
+
 public class HeapSort<E> extends ComparisonSorting<E> {
     @Override
     public void sort(final E[] array) {
