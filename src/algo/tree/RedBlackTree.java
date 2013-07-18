6c3bc50cd71e4408a9994eef47f5bc7123c77403
nodirt <turakulov@gmail.com>
2013-07-17 21:35:08 -0400

    AbstractRedBlackTree

--- src/algo/tree/RedBlackTree.java
+++ src/algo/tree/RedBlackTree.java
@@ -2,69 +2,12 @@ package algo.tree;
 
 import algo.tree.impl.*;
 
-public class RedBlackTree<K, V>
-        extends AbstractBalancedBinarySearchTree<K, V, RedBlackTree.Node<K, V>> {
+public class RedBlackTree<K, V> extends AbstractRedBlackTree<K, V, RedBlackTree.Node<K, V>> {
 
-    static class Node<K, V> extends AbstractBinarySearchTree.Node<K, V, Node<K, V>> {
-        boolean isRed;
-    }
+    static class Node<K, V> extends AbstractRedBlackTree.Node<K, V, Node<K, V>> {}
 
     @Override
     protected Node<K, V> createNode() {
         return new Node<K, V>();
-    }
-
-    boolean isRed(Node<K, V> node) {
-        return node != null && node.isRed;
-    }
-
-    boolean isBlack(Node<K, V> node) {
-        return node == null || !node.isRed;
-    }
-
-    @Override
-    protected boolean isBalanced(Node<K, V> node) {
-        return isBlack(node) || (isBlack(node.left) && isBlack(node.right));
-    }
-
-    @Override
-    protected Node<K, V> fixInsertion(Node<K, V> root, int direction) {
-        Node<K, V> changed = root.getChild(direction);
-        Node<K, V> unchanged = root.getChild(-direction);
-
-        if (isRed(changed)) {
-            if (isRed(unchanged)) {
-                // both children are red.
-                // Paint both black, and paint itself red
-                changed.isRed = false;
-                unchanged.isRed = false;
-                root.isRed = true;
-            } else {
-                if (isRed(changed.getChild(-direction))) {
-                    changed = rotate(changed, direction);
-                    root.setChild(direction, changed);
-                }
-
-                // the child is red, but its brother is black
-                changed.isRed = false;
-                root.isRed = true;
-                root = rotate(root, -direction);
-            }
-        }
-
-        return root;
-    }
-
-    @Override
-    public Node<K, V> insertNode(Node<K, V> node) {
-        node.isRed = true;
-        Node<K, V> result = super.insertNode(node);
-        getRoot().isRed = false;
-        return result;
-    }
-
-    @Override
-    public boolean supportsRemove() {
-        return false;
     }
 }
---
+++ src/algo/tree/impl/AbstractRedBlackTree.java
@@ -0,0 +1,63 @@
+package algo.tree.impl;
+
+public abstract class AbstractRedBlackTree<K, V, N extends AbstractRedBlackTree.Node<K, V, N>>
+        extends AbstractBalancedBinarySearchTree<K, V, N> {
+
+    public static class Node<K, V, N extends Node<K, V, N>> extends AbstractBinarySearchTree.Node<K, V, N> {
+        boolean isRed;
+    }
+
+    boolean isRed(N node) {
+        return node != null && node.isRed;
+    }
+
+    boolean isBlack(N node) {
+        return node == null || !node.isRed;
+    }
+
+    @Override
+    protected boolean isBalanced(N node) {
+        return isBlack(node) || (isBlack(node.left) && isBlack(node.right));
+    }
+
+    @Override
+    protected N fixInsertion(N root, int direction) {
+        N changed = root.getChild(direction);
+        N unchanged = root.getChild(-direction);
+
+        if (isRed(changed)) {
+            if (isRed(unchanged)) {
+                // both children are red.
+                // Paint both black, and paint itself red
+                changed.isRed = false;
+                unchanged.isRed = false;
+                root.isRed = true;
+            } else {
+                if (isRed(changed.getChild(-direction))) {
+                    changed = rotate(changed, direction);
+                    root.setChild(direction, changed);
+                }
+
+                // the child is red, but its brother is black
+                changed.isRed = false;
+                root.isRed = true;
+                root = rotate(root, -direction);
+            }
+        }
+
+        return root;
+    }
+
+    @Override
+    public N insertNode(N node) {
+        node.isRed = true;
+        N result = super.insertNode(node);
+        getRoot().isRed = false;
+        return result;
+    }
+
+    @Override
+    public boolean supportsRemove() {
+        return false;
+    }
+}
